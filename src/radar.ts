470fdac9e44b65b83720232abec8f5ca74b6bddc
artur <artur.krzynowek@sancus.com>
2022-08-08 16:26:43 +0100

    added test aimbot

--- src/radar.ts
+++ src/radar.ts
@@ -1,6 +1,6 @@
 import * as app from './lib';
-import {ui} from './ui';
-const canvas = <HTMLCanvasElement> document.querySelector('.canvas');
+import { ui } from './ui';
+const canvas = <HTMLCanvasElement>document.querySelector('.canvas');
 const radar = new app.features.Radar(canvas);
 
 canvas.addEventListener('dblclick', () => {
@@ -9,20 +9,21 @@ canvas.addEventListener('dblclick', () => {
     : document.body.requestFullscreen()).catch();
 });
 
-ui(x => renderAsync(x, new app.features.Sense()).finally(() => {
+ui(x => renderAsync(x, new app.features.Sense(), new app.features.Aimbot()).finally(() => {
   canvas.height = 0;
   canvas.width = 0;
 }));
 
-async function renderAsync(core: app.core.Core, sense: app.features.Sense) {
+async function renderAsync(core: app.core.Core, sense: app.features.Sense, aimbot: app.features.Aimbot) {
   await core.runAsync(() => {
     const levelName = core.levelName.value;
     const players = core.entityList.value;
     const localPlayer = players.find(x => x.address === core.localPlayer.value);
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    renderFrame(levelName, localPlayer, players),
+    renderFrame(levelName, localPlayer, players);
     updateSense(localPlayer, players, sense);
+    updateAimbot(levelName, localPlayer, players, aimbot);
   });
 }
 
@@ -31,7 +32,7 @@ function renderFrame(levelName: string, localPlayer: app.core.Player | undefined
     case 'mp_rr_canyonlands_staging':
       radar.refresh();
       if (!localPlayer) break;
-      radar.renderOne(localPlayer, {x: 31482.994140625, y: -6708.69677734375, z: 0}, '#FFF');
+      radar.renderOne(localPlayer, { x: 31482.994140625, y: -6708.69677734375, z: 0 }, '#FFF');
       break;
     default:
       radar.refresh();
@@ -43,6 +44,12 @@ function renderFrame(levelName: string, localPlayer: app.core.Player | undefined
 
 function updateSense(localPlayer: app.core.Player | undefined, players: Array<app.core.Player>, sense: app.features.Sense) {
   if (!localPlayer) return;
-  if (!location.hash.includes('enable-sense')) return;
+  if (!location.hash.includes('enable-sense') || !location.hash.includes('enable-sense-aimbot')) return;
   sense.updateStates(localPlayer, players);
+}
+
+function updateAimbot(levelName: string, localPlayer: app.core.Player | undefined, players: Array<app.core.Player>, aimbot: app.features.Aimbot) {
+  if (!localPlayer) return;
+  if (!location.hash.includes('enable-sense-aimbot')) return;
+  aimbot.updateStates(levelName, localPlayer, players);
 }
