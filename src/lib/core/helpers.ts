799789d9f6afe586e4ee62017de645ec4004329e
tea321000 <834914152@qq.com>
2022-08-28 01:59:48 +0800

    fix helper type

--- src/lib/core/helpers.ts
+++ src/lib/core/helpers.ts
@@ -1,12 +1,12 @@
-export function serialize<T>(item: T) {
+export function serialize(item: any) {
   return `(${Object.entries(item)
     .sort(([a], [b]) => a.localeCompare(b))
     .map(([k, v]) => `${k}=${v}`)
     .join(',')})`;
 }
 
-export function shallowEquals<T>(item: T, otherItem: T) {
+export function shallowEquals(item: any, otherItem: any) {
   return Object.entries(item)
-    .map(([k, v]) => [v, otherItem[k as keyof T]])
+    .map(([k, v]) => [v, otherItem[k as keyof any]])
     .every(([a, b]) => a === b);
 }
